9f9cf492423a64c0c95332164d4ecacccc05d3ed
Luqman <c0023@outlook.my>
2021-04-19 16:09:50 +0800

    commit

---
+++ apps/registration/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+class State(models.Model):
+    name = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.name
+
+class City(models.Model):
+    name = models.CharField(max_length=30)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+
+    first_name = models.CharField(max_length=100, null=True)
+    last_name = models.CharField(max_length=100, null=True)
+    iC = models.CharField(max_length=15, help_text='Format 780506-10-4554.')
+    date_of_birth = models.DateField(default=None)
+    email = models.EmailField(max_length=100, null=True)
+    adress_line_1 = models.CharField(max_length=100, null=True)
+    adress_line_2 = models.CharField(max_length=100, null=True)
+    poscode = models.CharField(max_length=5, null=True)
+    state = models.ForeignKey(State, on_delete=models.SET_NULL, null=True, default='none')
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, default='none')
+    contact_number = models.CharField(max_length=20, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\c0023\onedrive\documents\sites\project registration\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
