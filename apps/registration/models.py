ec9a49f38fa522b7be7543a7b0d4b3f29c225a3a
Luqman <c0023@outlook.my>
2021-04-19 16:33:15 +0800

    commit

--- apps/registration/models.py
+++ apps/registration/models.py
@@ -9,7 +9,7 @@ class State(models.Model):
 
 class City(models.Model):
     name = models.CharField(max_length=30)
-    state = models.ForeignKey(State, on_delete=models.CASCADE)
+    state = models.ForeignKey(State, on_delete=models.CASCADE, default='')
 
     def __str__(self):
         return self.name
@@ -19,14 +19,14 @@ class Student(models.Model):
 
     first_name = models.CharField(max_length=100, null=True)
     last_name = models.CharField(max_length=100, null=True)
-    iC = models.CharField(max_length=15, help_text='Format 780506-10-4554.')
+    # ic = models.CharField(max_length=15, help_text='Format 780506-10-4554.')
     date_of_birth = models.DateField(default=None)
     email = models.EmailField(max_length=100, null=True)
     adress_line_1 = models.CharField(max_length=100, null=True)
     adress_line_2 = models.CharField(max_length=100, null=True)
     poscode = models.CharField(max_length=5, null=True)
-    state = models.ForeignKey(State, on_delete=models.SET_NULL, null=True, default='none')
-    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, default='none')
+    state = models.ForeignKey(State, on_delete=models.SET_NULL, null=True, default='Please Select')
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, default='Please Select')
     contact_number = models.CharField(max_length=20, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
