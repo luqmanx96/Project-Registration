00c6188e1651671f133dc71d87dd90e9aff132f5
Luqman <c0023@outlook.my>
2021-04-20 08:38:37 +0800

    commit

--- p_main/urls.py
+++ p_main/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register', include('apps.registration.urls')),
+    path('', include('apps.registration.urls')), #add domain register/ later
 
 ]
